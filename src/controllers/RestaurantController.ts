db4f18efb8b9ab53bbefaa882b7246d8c0a129e6
muhaiminverse <muhaiminverse@gmail.com>
2025-02-03 18:57:01 +0600

    added checkout flow with stripe

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -2,6 +2,25 @@ import { error } from "console";
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async (req: Request, res: Response) => {
+    try {
+        const restaurantId = req.params.restaurantId;
+
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if(!restaurant){
+            res.status(404).json({ message: "restaurant not found"});
+            return;
+        }
+
+        res.json(restaurant);        
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({ message: "someting went wrong" });
+    }
+}
+
 const serachRestaurant = async (req: Request, res: Response) => {
     try {
         const city = req.params.city;
@@ -73,4 +92,5 @@ const serachRestaurant = async (req: Request, res: Response) => {
 
 export default {
     serachRestaurant,
+    getRestaurant,
 }
--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ import myUserRoute from "./routes/MyUserRoute"
 import {v2 as cloudinary} from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import RestaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 dotenv.config();
 
 const app = express(); // Middleware setup
-app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to database! Finally From Desktop"));
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => console.log("Connected to database! Finally From Desktop"));
 
 // cloudinary \\
 cloudinary.config({
@@ -23,6 +23,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "Health OK!" });
@@ -31,6 +34,7 @@ app.get("/health", async (req: Request, res: Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", RestaurantRoute);
+app.use("/api/order", orderRoute);
 
 
 app.get("/", (req: Request, res: Response) => {
